baf0280feaede66de97c119c2a612f06e71b6c39
Chapman6 <54628791+Chapman6@users.noreply.github.com>
2020-03-11 08:52:17 -0400

    Instructor Classes

---
+++ Instructor.cpp
@@ -0,0 +1,11 @@
+#include"instructor.h"
+
+Instructor::Instructor()
+{
+	Instructor.ID();
+}
+
+Instructor::~Instructor()
+{
+
+}
---
+++ Instructor.h
@@ -0,0 +1,11 @@
+#ifndef H_INSTRUCTOR
+#define	H_INSTRUCTOR
+#include "ID.h"
+class Instructor: public ID
+{
+public:
+	Instructor();
+	~Instructor();
+};
+
+#endif
