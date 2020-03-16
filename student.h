e73097bc0b9a420e140410a71b0228eee33793ec
lshinabery02@gmail.com <lshinabery02@gmail.com>
2020-03-16 14:44:23 -0400

    with student, no cpp written yet

---
+++ student.h
@@ -0,0 +1,32 @@
+#ifndef H_student
+#define H_student
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "ID.h"
+#include "course.h"
+
+using namespace std;
+
+const int noAssignments = 10;
+const int noCourses = 10;
+
+class student : protected ID
+{
+	struct courseInfo
+	{
+		course students[2];
+		float grades[noAssignments];
+	};
+	courseInfo studentCourses[noCourses];
+
+public:
+	void print();
+	void getInfo();
+	void setGrades();
+	student();
+	~student();
+};
+
+#endif
