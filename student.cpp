c7fc1359ff83aec1aeadb6cf73301b701001c9be
Madison Miller <madmill1602@gmail.com>
2020-03-18 08:54:27 -0400

    finish print function

--- student.cpp
+++ student.cpp
@@ -5,17 +5,77 @@
 #include<iomanip>
 using namespace std;
 
+student::student()
+{
+	int i, j;
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 10; j++)
+			grades[i][j] = 0;
+	}
+}
 void student::print()
 {
-	float average;
 	ID::printID();
+	int i = 0;
+	while (i < 2)
+	{
+		string courseName = course::setCourse();
+		cout << "Course: " << courseName << endl;
+		switch courseName
+		{
+		case "algebra":
+			cout << "The instructor is Mr. Math." << endl;
+			break;
+		case "biology":
+			cout << "The instructor is Mrs. Biology." << endl;
+			break;
+		case "calculus":
+			cout << "The instructor is Dr. Math." << endl;
+			break;
+		case "diving":
+			cout << "The instructor is Dr. Diving." << endl;
+			break;
+		case "english":
+			cout << "The instructor is Mr. Janke." << endl;
+			break;
+		case "greek":
+			cout << "The instructor is Mr. Greek." << endl;
+			break;
+		case "latin":
+			cout << "The instructor is Ms. Latin." << endl;
+			break;
+		case "metaphysics":
+			cout << "The instructor is Dr. Philosophy." << endl;
+			break;
+		case "physics":
+			cout << "The instructor is Mrs. Physics." << endl;
+			break;
+		case "soccer":
+			cout << "The instructor is Chapman." << endl;
+			break;
+		default:
+			cout << "The instructor is Mr. Lowe." << endl;
+		}
+		setGrades();
+		i++;
+	}
+}
+
+void student::setGrades()
+{
+	cout << "Please enter your grades." << endl;
 	int i;
+	float a, average;
+	cin >> a;
 	for (i = 0; i < 10; i++)
 	{
-		cout << grades[i] << " ";
-		average += grades[i];
+		cout << "Grade on assignment " << i << ": " << a << endl;
+		grades[0][i] = a;
+		average += a;
+		cin >> a;
 	}
-	cout << endl << "The average is " << average / 10 << endl;
-	string courseName = course::setCourse();
-	cout << "Course: " << courseName << endl;
+	cout << "Average grade: " << average / 10 << endl;
 }
+
+student::~student() {}
