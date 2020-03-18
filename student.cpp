0a3c7bc1b70d956345bce2cd5663afa8cb1f5e42
lshinabery02@gmail.com <lshinabery02@gmail.com>
2020-03-17 20:27:55 -0400

    commit1

---
+++ student.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include "student.h"
+#include "ID.h"
+#include "course.h"
+
+using namespace std;
+
+student::student()
+{
+	ID();
+	course::course();
+
+}
+
+student::~student()
+{
+
+}
