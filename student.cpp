dc796ad8d80fd74035849f0477fb55f54a319f2f
lshinabery02@gmail.com <lshinabery02@gmail.com>
2020-03-30 09:45:57 -0400

    dont use, unnecessary push

---
+++ student.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "ID.h"
+#include "course.h"
+#include "student.h"
+
+using namespace std;
+
+void student::print()
+{
+	printID();
+
+}
